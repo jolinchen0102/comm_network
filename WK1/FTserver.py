fe9d6aa8753885f1dcc226160f300365b5691ab0
jolinchen <jolinchen@Jolins-MacBook-Pro.local>
2022-02-09 09:14:53 +0800

    finish tcp comm

--- WK1/FTserver.py
+++ WK1/FTserver.py
@@ -1,38 +1,63 @@
-#!/usr/bin/python
+#!/Users/jolinchen/opt/anaconda3/bin/python
 
 import socket
 import sys
 
+ACKNOWLEDGEMENT = "OK"
+
+
 def main(argv):
-	# set port number
-	# default is 32341 if no input argument
+    server_port = argv[1] if len(argv) == 2 else 32341
+    
+    server_port = server_port if server_port else 32341
+    # set port number
+    # default is 32341 if no input argument
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockfd.bind(("", server_port))
 
+    # create socket and bind
+    print("I am", sockfd.getsockname(), "and I am listening")
 
-	# create socket and bind
+    # listen and accept new connection
+    sockfd.listen()
+    newsock, who = sockfd.accept()
 
+    # print out peer socket address information
+    print("A connection with", who, "has been established")
 
-	# listen and accept new connection
+    # receive file name, file size; and create the file
+    message = newsock.recv(1000)
 
+    # send acknowledge - e.g., "OK"
+    if message:
+        message = message.decode("ascii")
+        print(message+"\'", "is received from", who)
+        newsock.send(ACKNOWLEDGEMENT.encode("ascii"))
+    else:
+        print("connection is broken")
+    print(message, type(message))
+    filename, filesize = message.split(":")
+    filesize = int(filesize)
+    f = open("%s" % filename, "w")
 
-	# print out peer socket address information
+    # receive the file contents
+    print("Start receiving . . .")
 
+    received = 0
+    while received < filesize:
+        rmsg = newsock.recv(1000)
+        f.write(rmsg.decode("ascii"))
+        received += len(rmsg)
 
-	# receive file name, file size; and create the file
-
-
-	# send acknowledge - e.g., "OK"
-
-
-	# receive the file contents
-	print("Start receiving . . .")
-
-
-	# close connection
-	print("[Completed]")
-
+    # close connection
+    print("[Completed]")
+    f.close()
+    newsock.close()
+    sockfd.close()
+	
 
 if __name__ == '__main__':
-	if len(sys.argv) > 2:
-		print("Usage: FTserver [<Server_port>]")
-		sys.exit(1)
-	main(sys.argv)
+    if len(sys.argv) > 2:
+        print("Usage: FTserver [<Server_port>]")
+        sys.exit(1)
+    main(sys.argv)
--- WK1/Folder/FTclient.py
+++ WK1/Folder/FTclient.py
@@ -1,38 +1,74 @@
-#!/usr/bin/python
+#!/Users/jolinchen/opt/anaconda3/bin/python
 
 import socket
 import os.path
 import sys
 
+
 def main(argv):
+    _, server_addr, addr_port, filepath = argv
 
-	# open the target file; get file size
+    # open the target file; get file size
+    # filepath = "./test.txt"
+    try:
+        f = open(filepath, "r")
+        # content = f.read()
+        # f.close()
+        filesize = os.path.getsize(filepath)
+    except FileNotFoundError as err:
+        print(err)
+        sys.exit(1)
 
+    # create socket and connect to server
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        # print((server_addr, addr_port))
+        sockfd.connect((server_addr, int(addr_port)))
+    except socket.error as err:
+        print((server_addr, addr_port))
+        print("Connection error: ", err)
+        sys.exit(1)
 
-	# create socket and connect to server
+    # once the connection is set up; print out
+    # the socket address of your local socket
+    print("Connection established, local socket:", sockfd.getsockname())
 
+    # send file name and file size as one string separate by ':'
+    # e.g., socketprogramming.pdf:435678
+    try:
+        msg = os.path.basename(filepath)+":"+str(filesize)
+        print("sending message:", msg)
+        sockfd.send(msg.encode("ascii"))
+    except:
+        print("Terminated abnormally!!")
+        sockfd.close()
+        sys.exit(1)
+    # print("sending message:", msg)
+    # sockfd.send(msg.encode("ascii"))
 
-	# once the connection is set up; print out 
-	# the socket address of your local socket
+    # receive acknowledge - e.g., "OK"
+    try:
+        message = sockfd.recv(50)
+        print(message.decode("ascii"))
+    except socket.error as err:
+        print("Recv error: ", err)
+        sockfd.close()
+        sys.exit(1)
 
+    # send the file contents
+    print("Start sending ...")
+    while (filesize > 0):
+        blockLen = f.read(1000)
+        sockfd.send(blockLen.encode("ascii")) 
+        filesize -= len(blockLen)
+    f.close()
 
-	# send file name and file size as one string separate by ':'
-	# e.g., socketprogramming.pdf:435678
-
-
-	# receive acknowledge - e.g., "OK"
-
-
-	# send the file contents
-	print("Start sending ...")
-
-
-	# close connection
-	print("[Completed]")
-
+    # close connection
+    print("[Completed]")
+    sockfd.close()
 
 if __name__ == '__main__':
-	if len(sys.argv) != 4:
-		print("Usage: FTclient.py <Server_addr> <Server_port> <filename>")
-		sys.exit(1)
-	main(sys.argv)
+    if len(sys.argv) != 4:
+        print("Usage: FTclient.py <Server_addr> <Server_port> <filename>")
+        sys.exit(1)
+    main(sys.argv)
