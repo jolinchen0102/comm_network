81d2970f0caf9b08140b477ab0029341dc59592e
jolinchen <jolinchen@Jolins-MacBook-Pro.local>
2022-02-13 10:27:34 +0800

    add udp pingpong test

---
+++ udp/UDPpp.py
@@ -0,0 +1,42 @@
+
+# start: 40160, end: 40169
+from socket import *
+from time import perf_counter
+import sys
+
+def sender(argv):
+    serverIP, serverPort = argv[1], 40160
+    clientsocket = socket(AF_INET, SOCK_DGRAM)
+    msg = "1111"
+    for _ in range(10):
+        t_start = perf_counter()
+        clientsocket.sendto(msg.encode("ascii"), (serverIP, serverPort))
+        msg_r, _ = clientsocket.recvfrom(2048)
+        t_end = perf_counter()
+        print("Reply from %s: time = %.4f ms"%(msg_r.decode("ascii"), (t_end-t_start)*1000))
+    clientsocket.sendto("done".encode("ascii"), (serverIP, serverPort))
+    clientsocket.close()
+    
+
+def receiver():
+	serverPort = 40160
+	serversocket = socket(AF_INET, SOCK_DGRAM)
+	serversocket.bind(("", serverPort))
+	while True:
+		msg, clientAddress = serversocket.recvfrom(2048)
+		if msg.decode("ascii") != "done":
+			serversocket.sendto(msg, clientAddress)
+		else:
+			break
+	
+	
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        receiver()
+    elif len(sys.argv) == 2:
+        sender(sys.argv)
+    else:
+        print("To use as client program: python UDPpp.py <Server_addr>")
+        print("To use as server program: python UDPpp.py")
+        sys.exit(1)
