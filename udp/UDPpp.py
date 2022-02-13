a3fca6e62d945b655fca06c486cefbb284e0149a
jolinchen <jolinchen@Jolins-MacBook-Pro.local>
2022-02-13 10:48:02 +0800

    format

--- udp/UDPpp.py
+++ udp/UDPpp.py
@@ -3,6 +3,7 @@ from socket import *
 from time import perf_counter
 import sys
 
+
 def client(argv):
     serverIP, serverPort = argv[1], 40160
     clientsocket = socket(AF_INET, SOCK_DGRAM)
@@ -12,23 +13,23 @@ def client(argv):
         clientsocket.sendto(msg.encode("ascii"), (serverIP, serverPort))
         msg_r, _ = clientsocket.recvfrom(2048)
         t_end = perf_counter()
-        print("Reply from %s: time = %.4f ms"%(serverIP, (t_end-t_start)*1000))
+        print("Reply from %s: time = %.4f ms" %
+              (serverIP, (t_end-t_start)*1000))
     clientsocket.sendto("done".encode("ascii"), (serverIP, serverPort))
     clientsocket.close()
-    
+
 
 def server():
-	serverPort = 40160
-	serversocket = socket(AF_INET, SOCK_DGRAM)
-	serversocket.bind(("", serverPort))
-	while True:
-		msg, clientAddress = serversocket.recvfrom(2048)
-		if msg.decode("ascii") != "done":
-			serversocket.sendto(msg, clientAddress)
-		else:
-			break
-	
-	
+    serverPort = 40160
+    serversocket = socket(AF_INET, SOCK_DGRAM)
+    serversocket.bind(("", serverPort))
+    while True:
+        msg, clientAddress = serversocket.recvfrom(2048)
+        if msg.decode("ascii") != "done":
+            serversocket.sendto(msg, clientAddress)
+        else:
+            break
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
